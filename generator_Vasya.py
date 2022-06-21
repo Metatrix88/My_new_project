b9656d176c74b016c8fd49b93ffba40bc585032f
unknown <andrejdavidovich88@gmail.com>
2022-06-21 17:16:24 +0300

    Generator non stop

--- generator_Vasya.py
+++ generator_Vasya.py
@@ -1,12 +1,11 @@
 def gen_numbers():
-    for num in range(10):
+    num = 1
+    while True:
         if num % 3 != 0:
             yield num
         elif num % 3 == 0:
             yield 'Vasilii'
         num += 1
-
-
 get_num = gen_numbers()
 for i in get_num:
     print(i)
